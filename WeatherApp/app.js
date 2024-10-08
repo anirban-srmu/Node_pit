442dfb016dba4dfa14e2c5b744653dd883ce7045
anirban-srmu <anirban.srmu@gmail.com>
2024-10-08 15:41:39 +0530

    weather app login is added

--- WeatherApp/app.js
+++ WeatherApp/app.js
@@ -36,3 +36,24 @@ app.post('/register',async(req,res)=>{
         res.status(500).json({message:'Error creating user',error});
     }
 });
+
+// login API requires JWT
+const jwt = require('jsonwebtoken');
+app.post('/login', async(req,res)=>{
+    const {username,password,lat,lon} = req.body;
+
+    try {
+        const user = await User.findOne({username});
+        if (!user || !(await user.comparePassword(password))){
+            return res.status(401).json({message:' Invalid credentials'});
+        }
+
+        // Generate JWT token
+        const token = jwt.sign({id:user._id,role:user.role},process.env.JWT_SECRET,{expiresIn:'1h'});
+        res.json({token});
+
+    }catch (error){
+        console.error(error);
+        res.status(500).json({message:'Error login in',error});
+    }
+});
