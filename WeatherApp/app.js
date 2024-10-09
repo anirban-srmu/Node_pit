a8e6aba940468b77a2b1ec80004429228b35e322
anirban-srmu <anirban.srmu@gmail.com>
2024-10-09 10:42:13 +0530

    env is change and app uri is added

--- WeatherApp/app.js
+++ WeatherApp/app.js
@@ -19,7 +19,8 @@ mongoose.connect(process.env.MONGO_URI,{
 
 //Start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT,()=>console.log(`Server running on port ${PORT}`));
+const APP_URI = process.env.APP_URI;
+app.listen(PORT,()=>console.log(`Server is running at ${APP_URI}:${PORT}`));
 
 //import model User
 const User = require('./models/User')
